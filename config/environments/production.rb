efdf3f44749d1a33866622a13090d013205d11df
charlotte karimi <[charlotte.karimi@student.moringaschool.com]>
2023-04-06 01:29:22 +0300

    syntax errors corrections

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "blogging_app_ror_production"
+  # config.active_job.queue_name_prefix = "greener_gardens_production"
 
   config.action_mailer.perform_caching = false
 
